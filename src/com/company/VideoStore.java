032fc16c66f401cdda80728d712f3f378281dd3e
Manuespo <manuelesposito@outlook.com>
2023-04-03 21:35:35 -0300

    new methods added

--- src/com/company/VideoStore.java
+++ src/com/company/VideoStore.java
@@ -7,6 +7,8 @@ public class VideoStore {
     private ArrayList<Customer> customers=new ArrayList<Customer>();
     private ArrayList<Rent> rents=new ArrayList<Rent>();
 
+    public VideoStore() {
+    }
 
     public void addFilm(String title, String creationDate, int duration, int classification, String country, String description, Category category, int stock)
     {
@@ -16,37 +18,78 @@ public class VideoStore {
     {
         customers.add(new Customer(name,phoneNumber,address));
     }
-    public void rentByTitle(String title,String name, int phoneNumber, String address)
+    public void rentByTitle(String title,String name, int phoneNumber, String address, int rentdays)
     {
+        boolean founded=false;
         for (Film film:
              films) {
             if (title.equals(film.getTitle()))
             {
+                founded=true;
                 if (film.getStock()>0)
                 {
                     film.setStock(film.getStock()-1);
-                    if (findCustomer(new Customer(name,phoneNumber,address)));
-
-
+                    if (findCustomer(name,phoneNumber,address)!=null){
+                        rents.add(new Rent(findCustomer(name,phoneNumber,address),film,rentdays));
+                    }
+                    else {
+                        customers.add(new Customer(name,phoneNumber,address));
+                        rents.add(new Rent(customers.get(customers.size()-1),film,rentdays));
+                    }
                 }
-                else
-                    System.out.println("No more in Stock");
-
+                else {
+                    System.out.println("Out of stock");
+                }
             }
-            
         }
+        if (founded==false)
+            System.out.println("film not found");
+
     }
-    public boolean findCustomer(Customer customerToSearch)
+    public Customer findCustomer(String name, int phoneNumber, String address)
     {
-        boolean founded=false
+        boolean founded=false;
+        Customer aux=new Customer(name,phoneNumber,address);
         for (Customer customer: customers) {
-            if (customer.equals(customerToSearch)) {
+            if (customer.equals(aux)){
                 founded=true;
             }
         }
-        return founded;
+        if (founded==false)
+            return null;
+        else return aux;
     }
 
+    public ArrayList<Film> getFilms() {
+        return films;
+    }
 
+    public void setFilms(ArrayList<Film> films) {
+        this.films = films;
+    }
 
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(ArrayList<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public ArrayList<Rent> getRents() {
+        return rents;
+    }
+
+    public void setRents(ArrayList<Rent> rents) {
+        this.rents = rents;
+    }
+
+    @Override
+    public String toString() {
+        return "VideoStore{" +
+                "films=" + films +
+                ", customers=" + customers +
+                ", rents=" + rents +
+                '}';
+    }
 }
